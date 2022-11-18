c27d59bbaa54e69a7d1f84a5c4b4d310ebf4af7b
Universenn <dnwndhks98@gmail.com>
2022-11-18 15:04:17 +0900

    hospital 주소정보로 리스트 찾기

--- src/main/java/com/mustache/bbs3/controller/HospitalController.java
+++ src/main/java/com/mustache/bbs3/controller/HospitalController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/hospitals")
@@ -21,13 +22,39 @@ public class HospitalController {
         this.hospitalRepository = hospitalRepository;
     }
 
+//    @GetMapping("")
+//    public String list(Pageable pageable, Model model) {
+//        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
+//        log.info("size:{}", hospitals.getSize());
+//        model.addAttribute("hospitals", hospitals);
+//        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+//        model.addAttribute("next", pageable.next().getPageNumber());
+//        return "hospitals/list";
+//    }
+
     @GetMapping("")
-    public String list(Pageable pageable, Model model) {
-        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
-        log.info("size:{}", hospitals.getSize());
+    public String list(@RequestParam(value = "keyword", required = false) String keyword , Pageable pageable, Model model) {
+        // (@RequestParam String keyword, Pageable pageable,~
+        // 이게 강사님 코드. 내가 이렇게 실행하니 Required String parameter '인자' is not present 에러가 났다.
+        // required속성을 false값으로 줘서 해당 Parameter를 반드시 받지 않아도 되게 해줬다.
+        // keyword는 어떻게 받을 것인가?
+        log.info("keyword : {} ", keyword);
+        Page<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining(keyword, pageable);
         model.addAttribute("hospitals", hospitals);
+        model.addAttribute("keyword", keyword);
         model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
         model.addAttribute("next", pageable.next().getPageNumber());
         return "hospitals/list";
     }
+//    @GetMapping("")
+//    public String list(@RequestParam String keyword, Pageable pageable, Model model) {
+//        // keyword는 어떻게 받을 것인가?
+//        log.info("keyword:{}", keyword);
+//        Page<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining(keyword, pageable);
+//        model.addAttribute("hospitals", hospitals);
+//        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+//        model.addAttribute("next", pageable.next().getPageNumber());
+//        return "hospitals/list";
+//    }
+
 }
--- src/main/java/com/mustache/bbs3/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs3/repository/HospitalRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
     List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
     List<Hospital> findByRoadNameAddressContaining(String keyword);
+    Page<Hospital> findByRoadNameAddressContaining(String keyword, Pageable pageable);
     List<Hospital> findByHospitalNameStartsWith(String keyword);
     List<Hospital> findByHospitalNameEndsWith(String keyword);
     List<Hospital> findByPatientRoomCountGreaterThanAndPatientRoomCountLessThan(int var1, int var2);
